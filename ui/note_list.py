e86a96bc84b046523972054fe9dec1af2d4525ea
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-05-18 20:32:02 +0530

    Add NoteList class for displaying and searching notes in a treeview

---
+++ ui/note_list.py
@@ -0,0 +1,58 @@
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *  # noqa: F403
+
+
+class NoteList(ttk.Frame):
+    def __init__(self, parent, db, on_select):
+        super().__init__(parent, style="dark.TFrame")
+        self.db = db
+        self.on_select = on_select
+
+        # Search bar
+        self.search_var = ttk.StringVar()
+        self.search_entry = ttk.Entry(
+            self, textvariable=self.search_var, style="primary.Entry"
+        )
+        self.search_entry.pack(fill=X, padx=5, pady=5)
+        self.search_entry.bind("<Return>", self.on_search)
+
+        # Note list
+        self.note_tree = ttk.Treeview(
+            self, columns=("id", "title"), show="headings", style="primary.Treeview"
+        )
+        self.note_tree.heading("title", text="Notes")
+        self.note_tree.column("id", width=0, stretch=False)
+        self.note_tree.column("title", width=200)
+        self.note_tree.pack(fill=BOTH, expand=True, padx=5, pady=5)
+        self.note_tree.bind("<<TreeviewSelect>>", self.on_note_select)
+
+        self.load_notes()
+
+    def load_notes(self):
+        self.note_tree.delete(*self.note_tree.get_children())
+        notes = self.db.get_all_notes()
+        for note in notes:
+            self.note_tree.insert("", END, values=(note["id"], note["title"]))
+
+    def filter_by_tag(self, tag):
+        self.note_tree.delete(*self.note_tree.get_children())
+        notes = self.db.get_all_notes()
+        for note in notes:
+            if note["tags"] and tag in [t.strip() for t in note["tags"].split(",")]:
+                self.note_tree.insert("", END, values=(note["id"], note["title"]))
+
+    def on_search(self, event):
+        query = self.search_var.get().strip()
+        if query:
+            self.note_tree.delete(*self.note_tree.get_children())
+            notes = self.db.search_notes(query)
+            for note in notes:
+                self.note_tree.insert("", END, values=(note["id"], note["title"]))
+        else:
+            self.load_notes()
+
+    def on_note_select(self, event):
+        selected = self.note_tree.selection()
+        if selected:
+            note_id = self.note_tree.item(selected[0])["values"][0]
+            self.on_select(note_id)
